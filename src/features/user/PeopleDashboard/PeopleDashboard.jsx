303143e5c202ff8926444c330102ef16e4164014
jorge@jorge-PE60-6QE <moraleslopezjorge@gmail.com>
2018-08-11 18:49:11 +0200

    End of section 6

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PeopleDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PeopleDashboard</h1>
+      </div>
+    )
+  }
+}
+
+export default PeopleDashboard
